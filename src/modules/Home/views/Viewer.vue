db3f476e3c111619458a24f21867840f85377dbd
burhanahmeed <burhanahmeed@gmail.com>
2022-03-12 16:48:34 +0700

    feat: add player

--- src/modules/Home/views/Viewer.vue
+++ src/modules/Home/views/Viewer.vue
@@ -1,11 +1,20 @@
 <template>
   <Base>
-    <Page />
+    <Page>
+      <div v-if="videoId">
+        <youtube-iframe
+          :video-id="videoId"
+          player-width="100%"
+        />
+      </div>
+    </Page>
   </Base>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, onMounted, ref } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+
 import { Page, Base } from '@/src/components/ui';
 
 export default defineComponent({
@@ -13,6 +22,28 @@ export default defineComponent({
   components: {
     Base,
     Page,
+  },
+  setup() {
+    const route = useRoute();
+    const router = useRouter();
+
+    const videoId = ref<string|null>('');
+
+    onMounted(() => {
+      const { q } = route.query;
+
+      if (!q) {
+        router.push({
+          name: 'Home',
+        });
+      }
+
+      videoId.value = q || '';
+    });
+
+    return {
+      videoId,
+    };
   },
 });
 </script>
